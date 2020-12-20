1db39173be906d92bf0c356d691dc2d36d7b8172
rainbowunicornsj <74851527+rainbowunicornsj@users.noreply.github.com>
2020-12-20 16:58:57 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+tiles.setTilemap(tiles.createTilemap(hex`0a0008000202020202020202020402010101010101010104020101010101010101040201010101010101010402010101010101010105040404040404040404040303030303030303030603030303030303030303`, img`
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    `, [myTiles.transparency16,sprites.castle.tileGrass1,sprites.castle.tileGrass2,sprites.builtin.brick,sprites.dungeon.floorLight0,sprites.dungeon.doorClosedNorth,sprites.dungeon.stairLarge], TileScale.Sixteen))
+let mySprite = sprites.create(img`
+    . . . . . f f f f . . . . . 
+    . . . f f 5 5 5 5 f f . . . 
+    . . f 5 5 5 5 5 5 5 5 f . . 
+    . f 5 5 5 5 5 5 5 5 5 5 f . 
+    . f 5 5 5 d b b d 5 5 5 f . 
+    f 5 5 5 b 4 4 4 4 b 5 5 5 f 
+    f 5 5 c c 4 4 4 4 c c 5 5 f 
+    f b b f b f 4 4 f b f b b f 
+    f b b 4 1 f d d f 1 4 b b f 
+    . f b f d d d d d d f b f . 
+    . f e f e 4 4 4 4 e f e f . 
+    . e 4 f 6 9 9 9 9 6 f 4 e . 
+    . 4 d c 9 9 9 9 9 9 c d 4 . 
+    . 4 f b 3 b 3 b 3 b b f 4 . 
+    . . f f 3 b 3 b 3 3 f f . . 
+    . . . . f f b b f f . . . . 
+    `, SpriteKind.Player)
